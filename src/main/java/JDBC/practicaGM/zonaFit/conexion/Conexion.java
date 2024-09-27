800e41139e1875a177229fa9c5ecc44938058bca
David Torres <davidtorresdev7@gmail.com>
2024-09-27 00:34:30 -0500

    Clase conexion proyecto ZonaFit

---
+++ src/main/java/JDBC/practicaGM/zonaFit/conexion/Conexion.java
@@ -0,0 +1,40 @@
+package JDBC.practicaGM.zonaFit.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+    public static Connection getConexion() {
+
+        Connection newConnection = null;
+        String database = "zonaFitDB";
+        String url = "jdbc:mysql://localhost:3306/" + database;
+        String username = "root";
+        String password = "root";
+
+        try {
+            //Cargando la clase en memoria
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //Estableciendo la conexion a la base de datos
+            newConnection = DriverManager.getConnection(url, username, password);
+
+        } catch (Exception e) {
+            System.out.println("Se genero un error de tipo: " + e.getClass().getName());
+            System.out.println("Motivo: " + e.getMessage());
+            System.out.println("--------------------------------");
+            throw new RuntimeException(e);
+        }
+
+        return newConnection;
+    }
+
+    public static void main(String[] args) {
+
+        Connection comm = Conexion.getConexion();
+
+        System.out.println("Conexion: " + comm);
+    }
+
+}
